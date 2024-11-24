d3470ff08c3cc3d043f3ed7ca4a25f1fa2099c55
Ingo Ruhnke <grumbel@gmail.com>
2024-11-24 22:45:44 +0100

    Update to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     scummvm-tools_src.url = "github:scummvm/scummvm-tools?ref=v2.7.0";
